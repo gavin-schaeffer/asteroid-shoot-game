fe80a2f569b1983980c770efd754c51d45e18d75
gavin.schaeffer <gavin.schaeffer@international.com>
2026-02-05 16:17:43 -0600

    updating log_state in main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     while True:
-        for event in pygame.event.get():
-            pass
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
